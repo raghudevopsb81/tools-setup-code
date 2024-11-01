ef7c6096abd7a23309d15665c91f94da7dbda23c
SVR Kalluru <raghuk.vit@gmail.com>
2024-11-01 08:09:12 +0530

    Adding print statements

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "tools" {
       port          = 80 # Just a dummy port
       volume_size   = 20
       instance_type = "t3.small"
-      policy_list = ["ec2:*", "route53:*"]
+      policy_list = ["*"]
     }
 
   }
