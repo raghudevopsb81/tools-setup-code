de91d80c5727639f1f198728b282a4366280a605
Raghu K <raghuk.vit@gmail.com>
2024-12-17 08:44:07 +0530

    RoboShop

--- variables.tf
+++ variables.tf
@@ -2,21 +2,26 @@ variable "tools" {
   default = {
 
     vault = {
-      port          = 8200
+      port          = {
+        vault_port  = 8200
+      }
       volume_size   = 20
       instance_type = "t3.small"
       policy_list = []
     }
 
     github-runner = {
-      port          = 80 # Just a dummy port
+      port          = {}
       volume_size   = 50
       instance_type = "t3.small"
       policy_list = ["*"]
     }
 
     elasticsearch = {
-      port          = 80
+      port          = {
+        elasticsearch = 9200
+        nginx         = 80
+      }
       volume_size   = 50
       instance_type = "r6idn.large"
       policy_list = []
